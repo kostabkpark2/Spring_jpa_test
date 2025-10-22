9ee7ee87b484713beb9fa8770cb53388f8d88264
kostabkpark2 <kostabkpark2@gmail.com>
2025-10-22 15:59:12 +0900

    first commit

---
+++ src/test/java/org/example/spring_jpa_test/SpringJpaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.spring_jpa_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaTestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
